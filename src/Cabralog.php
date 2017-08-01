5fb09983e2e23c7675cc370477edcc732716a27c
Carlos Rodríguez <carlosrodriguez@xoborg.com>
2017-08-01 10:07:54 +0200

    First commit.

---
+++ src/Cabralog.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the cabralog php package.
+ *
+ * (c) Xoborg <developers@xoborg.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xoborg\Cabralog;
+
+class Cabralog{
+
+}
